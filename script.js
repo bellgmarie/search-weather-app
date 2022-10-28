c426d8ea405327800a0cc5ba9a50696aa636c3c2
bell <108286428+bellgmarie@users.noreply.github.com>
2022-10-28 12:53:43 -0400

    cleaning script and html

--- script.js
+++ script.js
@@ -69,6 +69,8 @@ function defaultTemperature(response) {
 
 let apiKey = "f0fc91db3aoa04a9t8419fe6b4378f88";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Tokyo&key=${apiKey}&units=imperial`;
+/* let apiKey = `c3a3993027e6129d50f3eb164a7e386a`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=${apiKey}`; */
 axios.get(apiUrl).then(defaultTemperature);
 
 // end default
